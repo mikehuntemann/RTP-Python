52c38e32e2896b943bacafc931990c0120e71c77
Mike Huntemann <mail@mikehuntemann.de>
2015-12-07 18:31:58 +0100

    config implementation & fixes
    implementation of external variables, new function names for readability, bug fixes on mongo.saveUrl() logic, preventing duplicats in mongodb.

--- YoutubeSearch.py
+++ YoutubeSearch.py
@@ -19,70 +19,73 @@ import subtitleDownloader
 #import analysingData
 
 
-
-API_KEY = "AIzaSyAjrnPLRyykFySLHfsrfz9SS7l8p--Rnjg"
-keyword = "NSA"
-dbName = 'youtube'
-headers = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)'}
+with open("settings.json") as settings_file:
+	variables = json.load(settings_file)
 
 
-def build_new_source():
+API_KEY = variables["API_KEY"]
+SEARCH_KEY = variables["SEARCH_KEY"]
+DB_KEY = variables["DB_KEY"]
+HEADER = variables["HEADER"]
+GOOGLE_API_BASE = variables["GOOGLE_API_BASE"]
+YOUTUBE_BASE = variables["YOUTUBE_BASE"]
+
+
+def buildNewSource():
 	while (mongo.getNotPicked() != 0):
 		try:
 			tinyurl = mongo.getRandomID()
-			new_url ='http://www.youtube.com/watch?v='+tinyurl
-			print "new url is "+new_url
-			get_all_links(new_url)
+			url = YOUTUBE_BASE + tinyurl
+			print "now crawling " + url + "."
+			getAllLinks(url)
 			mongo.pickUpdate(tinyurl)
 		except:
 			continue
 
-def get_site_html(url):
+def getSiteHtml(url):
 	respone = None
 	opener = urllib2.build_opener()
 	try:
-		request = urllib2.Request(url,None,headers)
+		request = urllib2.Request(url, None, HEADER)
 		response = opener.open(request).read()
 	except:
 		pass	
 	return response
 	
 
-def get_all_links(url):
+def getAllLinks(url):
 	links = []
-	i = 0
-	source = get_site_html(url)
-	links = re.findall('\/watch\?v=\w{11}', source)
-	for link in links:
+	counter = 0
+	source = getSiteHtml(url)
+	allLinks = re.findall('\/watch\?v=\w{11}', source)
+	for link in allLinks:
 		if (link.startswith('/watch?v=') == True):
 			tinyurl = link.split('=')[-1]
-			if tinyurl in links:
-				continue
-			links.append(tinyurl) 
-			mongo.saveUrl(tinyurl)
-			infoSearch(tinyurl)
-			i += 1
-	
-	print 'done saving in db!'
+			if tinyurl not in links:
+				if (mongo.saveUrl(tinyurl)):
+					allLinks.append(tinyurl) 
+					getDataFromVideo(tinyurl)
+					counter += 1
+	print counter + " videos added to Mongodb."
 
 
-def infoSearch(tinyurl):
-	new_url = 'https://www.googleapis.com/youtube/v3/videos?id='+tinyurl+'&key='+API_KEY+'&fields=items(id,snippet(title,description),contentDetails(caption))&part=snippet,contentDetails'
-	response = get_site_html(new_url)
+def getDataFromVideo(tinyurl):
+	url = GOOGLE_API_BASE+tinyurl+'&key='+API_KEY+'&fields=items(id,snippet(title,description),contentDetails(caption))&part=snippet,contentDetails'
+	response = getSiteHtml(url)
 	dataset = json.loads(response)
 	for data in dataset['items']:
 		title = data['snippet']['title']
 		description = data['snippet']['description']
-		specificSearch(title, description, tinyurl)
+		checkContentForMatch(title, description, tinyurl)
 
 
-def specificSearch(title, description, tinyurl):
-	if (re.findall(keyword, title)):
+def checkContentForMatch(title, description, tinyurl):
+	if (re.findall(SEARCH_KEY, title)):
 		print "match in title"
 		mongo.titleUpdate(title, tinyurl)
 		mongo.infoUpdate(tinyurl)
 		subtitleDownloader.getCaption(tinyurl)
-	elif (re.findall(keyword, description, re.MULTILINE)):
+	elif (re.findall(SEARCH_KEY, description, re.MULTILINE)):
 		print "match in description"
 		mongo.titleUpdate(title, tinyurl)
 		mongo.infoUpdate(tinyurl)
@@ -100,8 +103,8 @@ if __name__ == '__main__':
 	subtitleDownloader.init(mongo)
 
 	# search
-	#get_all_links('http://www.youtube.com/results?search_query='+keyword)
-	build_new_source()
+	getAllLinks('http://www.youtube.com/results?search_query='+SEARCH_KEY)
+	#buildNewSource()
 	
 
 
