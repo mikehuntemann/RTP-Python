c4c1392c8e0b9da96ef7d1c140bdc191f444ac78
Mike Huntemann <mail@mikehuntemann.de>
2016-01-08 16:48:58 +0100

    added sub and snippet match and merge

--- mergeFiles.py
+++ mergeFiles.py
@@ -2,12 +2,44 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
+import shlex
+import subprocess
+
+sys.path.append(os.path.abspath("lib"))
+import mongo
 
 
-def findMatchingFiles():
-    for subFile in os.listdir("exports/subs"):
-        print subFile
+counter = 0
 
-    for snippetFile in os.listdir("exports/snippets"):
-        print snippetFile
+def loadSnippetsToDb():
+	for file in os.listdir("exports/snippets/"):
+		if file.endswith(".mp4"):
+            filename = file.split(".")[0]
+            for subfile in os.listdir("exports/subs/"):
+                if (subfile.split(".")[0] == filename):
+                    mongo.updateSubAndSnippet(filename)
+                    counter =+ 1
+            else:
+                print file+" is no .mp4 file."
+    print counter + " matches added to mongoDB."
 
+def bakeSubs():
+    dataset = mongo.findKeyword("")
+    print dataset.count()
+    for filename in dataset:
+        try:
+            commandline = "./ffmpeg -i exports/snippets/"+filename+".mp4 -vf ass=exports/subs/"+filename+".ass" "exports/baked_files/"+filename+"_sub.mp4"
+            snippet = shlex.split(commandline)
+            subprocess.Popen(snippet)
+        except:
+            pass
+
+    print "all videos baked."
+
+
+if __name__ == '__main__':
+    # mongo.init()
+    mongo.dropAndReconnect()
+    loadSnippetsToDb()
+    bakeSubs()
